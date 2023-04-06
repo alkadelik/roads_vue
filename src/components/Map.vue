06db65a827924a90409c9d128b45dd42026e5ee4
Adebola Adeola <alkadelik@Adebolas-MacBook-Pro.local>
2023-04-06 14:11:48 +0100

    added closing button to segment map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="">
+    <p><span @click="hideMap">x</span></p>
     <img :src="'http://127.0.0.1:8000' + map.map" alt="segment map">
     <h4>{{ map.segment }}</h4>
 
@@ -8,11 +9,18 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import store from '@/store/index'
+import * as mutationTypes from '@/store/mutationTypes'
 
 export default {
   name: 'MapComp',
   props: {
     // msg: String
+  },
+  methods: {
+    hideMap() {
+      store.commit(mutationTypes.HIDE_MAP, false)
+    }
   },
   computed: {
     ...mapGetters({
@@ -26,6 +34,18 @@ export default {
   img {
     width: 400px;
     /* margin-top: 100px; */
+  }
+  p {
+    text-align: right;
+    margin: 0;
+    font-size: 24px;
+  }
+  span {
+    font-weight: bold;
+    cursor: pointer;
+  }
+  h4 {
+    margin-top: 0;
   }
 
 </style>
